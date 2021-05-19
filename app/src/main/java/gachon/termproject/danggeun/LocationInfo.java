df77af0863099509cac7626177e206002d6b3252
chws0508 <wooseokchoi0508@gmail.com>
2021-05-19 23:36:26 +0900

    Map 부분 추가

---
+++ app/src/main/java/gachon/termproject/danggeun/LocationInfo.java
@@ -0,0 +1,30 @@
+package gachon.termproject.danggeun;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LocationInfo {
+
+    private LatLng latLng;
+    String markerTitle;
+    String markerSnippet;
+
+    public LocationInfo() {}
+
+    public LocationInfo(LatLng latLng, String markerTitle, String markerSnippet){
+        this.latLng=latLng;
+        this.markerTitle=markerTitle;
+        this.markerSnippet=markerSnippet;
+    }
+
+    public LatLng getLatLng(){return latLng;}
+    public String getMarkerTitle(){
+        return markerTitle;
+    }
+    public String getMarkerSnippet() {return markerSnippet;}
+
+    public void setLatLng(LatLng latLng){this.latLng=latLng;}
+    public void setMarkerTitle(String markerTitle){this.markerTitle=markerTitle;}
+    public void setMarkerSnippet(String markerSnippet){this.markerSnippet=markerSnippet;}
+
+
+}
