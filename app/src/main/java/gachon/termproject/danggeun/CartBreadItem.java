986fd9ee05934bf162c5d8ec548f5fed7e44a7a7
miiiin0 <79246888+miiiin0@users.noreply.github.com>
2021-05-20 01:41:04 +0900

    장바구니 기능 추가

---
+++ app/src/main/java/gachon/termproject/danggeun/CartBreadItem.java
@@ -0,0 +1,48 @@
+package gachon.termproject.danggeun;
+
+public class CartBreadItem {
+    String cartBreadName;
+    String cartBreadPrice;
+    String cartTotalPrice;
+    int cartCount;
+
+    // Generate > Constructor
+    public CartBreadItem(String name, String price, String totalPrice, int count) {
+        this.cartBreadName = name;
+        this.cartBreadPrice = price;
+        this.cartTotalPrice = totalPrice;
+        this.cartCount = count;
+    }
+
+    // Generate > Getter and Setter
+    public String getCartBreadName() {
+        return cartBreadName;
+    }
+    public void setCartBreadName(String name) {
+        this.cartBreadName = name;
+    }
+
+    public String getCartBreadPrice() {
+        return cartBreadPrice;
+    }
+    public void setCartBreadPrice(String price) {
+        this.cartBreadPrice = price;
+    }
+
+    public String getCartTotalPrice() {
+        return cartTotalPrice;
+    }
+    public void setCartTotalPrice(String totalPrice) {
+        this.cartTotalPrice = totalPrice;
+    }
+
+    public int getCartCount() {
+        return cartCount;
+    }
+    public void setCartCount(int count) {
+        this.cartCount = count;
+    }
+
+
+
+}
