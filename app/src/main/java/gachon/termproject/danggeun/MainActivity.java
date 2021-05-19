650bac108d15f800418fe0527f3d337b686d880f
miiiin0 <79246888+miiiin0@users.noreply.github.com>
2021-05-19 20:57:24 +0900

    minyoung

--- app/src/main/java/gachon/termproject/danggeun/MainActivity.java
+++ app/src/main/java/gachon/termproject/danggeun/MainActivity.java
@@ -1,8 +1,11 @@
 package gachon.termproject.danggeun;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.bundle);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //빵 정보 번들로 보내기
+                Bundle bundle = new Bundle();
+                bundle.putString("store", "파리바게트 00점");
+                bundle.putString("img", "Bread1");
+                bundle.putString("name", "초코소라빵");
+                bundle.putString("price", "1200");
+                bundle.putString("maximum", "5");
+
+                Intent intent = new Intent(MainActivity.this, Bread_Detail.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
     }
 }
