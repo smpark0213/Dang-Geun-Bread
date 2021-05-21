1740554c7de5b38d3a19f15ff0be4c3f3e06840f
smpark0213 <smpark0213@naver.com>
2021-05-21 18:48:52 +0900

    회원가입 빵집정보 추가 & isConsumer오류 해결

--- app/src/main/java/gachon/termproject/danggeun/Signup/Signup04Activity.java
+++ app/src/main/java/gachon/termproject/danggeun/Signup/Signup04Activity.java
@@ -12,8 +12,10 @@ import gachon.termproject.danggeun.R;
 
 public class Signup04Activity extends AppCompatActivity {
     public static String location = "";
+    public static String bakery = "";
     private Button nextButton;
     private EditText text;
+    private EditText text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class Signup04Activity extends AppCompatActivity {
         setContentView(R.layout.signup04_location);
 
         text = findViewById(R.id.signup04_edittext01);
+        text1 = findViewById(R.id.signup04_edittext01_user);
         nextButton = findViewById(R.id.signup04_button01);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +31,7 @@ public class Signup04Activity extends AppCompatActivity {
             public void onClick(View v) {
 
                 location = text.getText().toString();
+                bakery = text1.getText().toString();
                 Intent intent = new Intent(getApplicationContext(), Signup06Activity.class);
                 startActivity(intent);
 
