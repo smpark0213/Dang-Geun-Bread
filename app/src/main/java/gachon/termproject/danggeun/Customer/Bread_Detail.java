55386ab1e57f0370dbf1c78c3fcc75c234cacf93
ucans <zyzy1231@naver.com>
2021-05-23 23:38:12 +0900

    Cart Dialog

--- app/src/main/java/gachon/termproject/danggeun/Customer/Bread_Detail.java
+++ app/src/main/java/gachon/termproject/danggeun/Customer/Bread_Detail.java
@@ -3,6 +3,7 @@ package gachon.termproject.danggeun.Customer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -24,6 +25,7 @@ import com.google.firebase.storage.StorageReference;
 import gachon.termproject.danggeun.LoginActivity;
 import gachon.termproject.danggeun.R;
 import io.grpc.InternalNotifyOnServerBuild;
+import android.app.AlertDialog;
 
 public class Bread_Detail extends AppCompatActivity {
 
@@ -38,6 +40,34 @@ public class Bread_Detail extends AppCompatActivity {
     private String breadName;
     private String breadPrice;
     private String maximum;
+
+    private AlertDialog alertDialog = null;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(alertDialog != null && alertDialog.isShowing()){
+            alertDialog.dismiss();
+        }
+    }
+
+    public void createDialog(){
+        alertDialog = new AlertDialog.Builder(Bread_Detail.this)
+                .setTitle("같은 가게의 메뉴만 담을 수 있습니다.")
+                .setMessage("주문할 가게를 변경하실 경우 이전에 담은 메뉴가 삭제됩니다.").setPositiveButton("새로 담기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 카트 클리어
+                        StoreActivity.clearCart();
+                        BreadDTO br = new BreadDTO(breadName, breadPrice, Integer.toString(counter), Integer.toString(totalPrice));
+                        Intent reIntent = new Intent();
+                        reIntent.putExtra("toCart", br);
+                        setResult(RESULT_OK, reIntent);
+                    }
+                })
+                .setNegativeButton("취소", null).setCancelable(false).create();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +97,6 @@ public class Bread_Detail extends AppCompatActivity {
 
 
             //Toast.makeText(getApplicationContext(), breadName, Toast.LENGTH_SHORT).show();
-
         }
 
         //빵사진 파베 storage에 있는 빵 사진 가져와야함
@@ -152,6 +181,26 @@ public class Bread_Detail extends AppCompatActivity {
         cartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { //파이어베이스에 올리기
+                // 카트에 빵이 있다면,
+                if(StoreActivity.isCart()){
+                    // 같은 가게라면,
+                    if(StoreActivity.getCartStore().equals(storeName)){
+                        BreadDTO br = new BreadDTO(breadName, breadPrice, Integer.toString(counter), Integer.toString(totalPrice));
+                        Intent reIntent = new Intent();
+                        reIntent.putExtra("toCart", br);
+                        setResult(RESULT_OK, reIntent);
+                        finish();
+                    }
+                    else{
+                        // 다른 가게,
+                        // 카트를 비우고 현재 가게의 빵을 추가할 것인지,
+                        // 카트를 비우지 않을 것인지 선택하게 할 것.
+                        createDialog();
+                        alertDialog.show();
+                        finish();
+                    }
+                }
+                // 카트에 빵이 없는 경우, 그냥 추가
                 BreadDTO br = new BreadDTO(breadName, breadPrice, Integer.toString(counter), Integer.toString(totalPrice));
                 Intent reIntent = new Intent();
                 reIntent.putExtra("toCart", br);
@@ -169,6 +218,4 @@ public class Bread_Detail extends AppCompatActivity {
         });
 
     }
-
-
 }
